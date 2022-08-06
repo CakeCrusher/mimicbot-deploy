5ce500f07f9896367ab91a4331ca27859e53fe58
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-08-06 15:39:16 -0400

    adapted to mimicbot_chat depenancy

--- mimic.py
+++ mimic.py
@@ -10,6 +10,7 @@ import pdb
 from pathlib import Path
 import typer
 import datetime
+import mimicbot_chat.utils as mimicbot_chat
 import dotenv
 dotenv.load_dotenv()
 
@@ -31,45 +32,6 @@ def start_mimic():
     intents.members = True
     intents.messages = True
     bot = commands.Bot(intents=intents, command_prefix="!")
-
-    def messages_into_input(messages, members_df):
-        messages_df_columns = ["content", "timestamp", "channel"]
-        context_data = [
-            [message.content, message.created_at, message.channel.name]
-            for message in messages
-        ]
-
-        # remove first mention of bot from each message
-        for idx, context_data_ins in enumerate(context_data):
-            message = context_data_ins[0]
-            bot_id_decorated = f"<@{bot.user.id}>"
-            split_by_bot_id = message.split(bot_id_decorated)
-            start_of_message = split_by_bot_id[0]
-            rest_of_message = bot_id_decorated.join(split_by_bot_id[1:])
-            new_message = (start_of_message + rest_of_message).strip()
-            context_data[idx][0] = new_message
-
-        context_df = pd.DataFrame(
-            columns=messages_df_columns, data=context_data)
-        context_df = data_preprocessing.clean_df(context_df, members_df)
-
-        return EOS_TOKEN.join(list(context_df["content"])) + EOS_TOKEN
-
-    def query(payload_input):
-        headers = {"Authorization": f"Bearer {HF_TOKEN}"}
-        API_URL = f"https://api-inference.huggingface.co/models/{MODEL_ID}"
-        messages_list = payload_input.split(EOS_TOKEN)[:-1]
-        payload = {
-            "inputs": {
-                "past_user_inputs": messages_list[:-1],
-                "generated_responses": [],
-                "text": messages_list[-1],
-            }
-        }
-        payload_dump = json.dumps(payload)
-        response = requests.request(
-            "POST", API_URL, headers=headers, data=payload_dump)
-        return json.loads(response.content.decode("utf-8"))
 
     @bot.event
     async def on_ready():
@@ -95,36 +57,18 @@ def start_mimic():
                 typer.secho(f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) {message.author} mentioned me",
                             fg=typer.colors.BLUE)
                 context_messages = await channel.history(limit=int(AMT_OF_CONTEXT)).flatten()
-                payload_text = messages_into_input(
-                    context_messages, members_df)
-                typer.echo(
-                    f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) {payload_text}")
-                query_res = query(payload_text)
-                attempts = 0
-                typer.echo(
-                    f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) {query_res}")
-                while "error" in query_res.keys() and attempts <= 3:
-                    # wait for model to load and try again
-                    time_to_load = int(int(query_res["estimated_time"]) * 1.3)
+                context_messages = [
+                    message.content for message in context_messages]
+                
+                def temp_respond(response):
                     typer.secho(
-                        f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) Waiting for model to load. Will take {time_to_load}s", fg=typer.colors.YELLOW)
-                    await asyncio.sleep(time_to_load)
-                    query_res = query(payload_text)
-                    typer.echo(
-                        f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) {query_res}")
-                    attempts += 1
-                if attempts > 3:
-                    typer.secho(
-                        f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) Model failed to load. Please try again later.", fg=typer.colors.RED)
-                    await channel.send("🤖(failed to load, please try again later)")
-                else:
-                    response = query_res["generated_text"]
-                    try:
-                        await channel.send(response)
-                    except discord.errors.HTTPException:
-                        typer.secho(
-                            f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) HTTPException error.", fg=typer.colors.YELLOW)
-                        await channel.send("...")
+                        f"\n({datetime.datetime.now().hour}:{datetime.datetime.now().minute}) {response}", fg=typer.colors.YELLOW)
+
+                async def respond(response):
+                    await channel.send(response)
+                
+                await mimicbot_chat.respond_to_message(context_messages, members_df, respond, temp_respond, MODEL_ID, HF_TOKEN, EOS_TOKEN, platform='discord', bot=bot)
+                
 
     bot.run(MIMICBOT_TOKEN)
 
